1f6996ba32439dba66979e79130e4e9aa240e891
Yohan Morales <yohanmorales@hotmail.com>
2015-12-30 16:09:05 -0500

    + adding rq queue manager and redis server

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,21 @@
 apt-get update
 apt-get -y install build-essential
 apt-get -y install python-dev
+apt-get -y install tcl8.5
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install psutil
 pip install pyyaml
+pip install rq
+
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+make install
+cd utils
+bash install_server.sh
+service redis_6379 start
+
